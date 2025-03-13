43e71ac667fca2dfb1e1cb727719a0b64108f5dd
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2025-03-13 22:04:58 +0900

    feat: 시도 회수 만큼 라운드 진행

--- racingcar/controller/racing_game_controller.go
+++ racingcar/controller/racing_game_controller.go
@@ -8,13 +8,21 @@ import (
 func Run() {
 	cars := readCars()
 	trialCount := ui.ReadTrialCount()
+	game := carModels.NewRacingGame(cars, trialCount)
+	playGame(game)
 }
 
-func readCars() carModels.Cars {
+func readCars() *carModels.Cars {
 	carNamesInput := ui.ReadCars()
 	cars, err := carModels.NewCarsFromNames(carNamesInput)
 	if err != nil {
 		panic(err)
 	}
 	return cars
+}
+
+func playGame(game carModels.RacingGame) {
+	for i := 0; i < game.TrialCount; i++ {
+		game.PlaySingleRound()
+	}
 }
