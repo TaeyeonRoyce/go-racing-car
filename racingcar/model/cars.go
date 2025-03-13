bc7f8255b31316dd04f69e7734c8491c06d6235f
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2025-03-13 19:15:08 +0900

    feat: 자동차 일급 컬렉션 생성

---
+++ racingcar/model/cars.go
@@ -0,0 +1,33 @@
+package model
+
+import "errors"
+
+var _duplicateCarNameError = errors.New("중복된 자동차 이름이 존재합니다")
+
+type Cars []*Car
+
+func NewCarsFromNames(names []string) (Cars, error) {
+	cars := make(Cars, len(names))
+	for i, name := range names {
+		car, err := NewCar(name)
+		if err != nil {
+			return nil, err
+		}
+		cars[i] = car
+	}
+	if err := validateDuplicateCarNames(cars); err != nil {
+		return nil, err
+	}
+	return cars, nil
+}
+
+func validateDuplicateCarNames(cars Cars) error {
+	uniqueCarNames := make(map[CarName]bool)
+	for _, car := range cars {
+		if _, ok := uniqueCarNames[car.Name]; ok {
+			return _duplicateCarNameError
+		}
+		uniqueCarNames[car.Name] = true
+	}
+	return nil
+}
---
+++ racingcar/model/cars_test.go
@@ -0,0 +1,51 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewCarsFromNames(t *testing.T) {
+	testCases := []struct {
+		names            []string
+		expectedCarCount int
+		expectedError    error
+	}{
+		{[]string{"a", "b"}, 2, nil},
+		{[]string{"a", "b", "c", "d", "e"}, 5, nil},
+		{[]string{"abcedfg", "f"}, 0, _invalidCarNameLengthError},
+	}
+
+	for _, test := range testCases {
+		t.Run("", func(t *testing.T) {
+			cars, err := NewCarsFromNames(test.names)
+			assert.ErrorIs(t, err, test.expectedError)
+
+			if err == nil {
+				assert.Equal(t, test.expectedCarCount, len(cars))
+			}
+		})
+	}
+}
+
+func TestValidateDuplicationCarNames(t *testing.T) {
+	testCases := []struct {
+		names         []string
+		expectedError error
+	}{
+		{[]string{"a", "a"}, _duplicateCarNameError},
+		{[]string{"a", "b"}, nil},
+	}
+
+	for _, test := range testCases {
+		t.Run("", func(t *testing.T) {
+			cars, err := NewCarsFromNames(test.names)
+			assert.ErrorIs(t, err, test.expectedError)
+
+			if err == nil {
+				assert.Equal(t, len(cars), len(cars))
+			}
+		})
+	}
+}
